a7127043e2026538528fcd99e0d5633bfe206ecb
ozodbeknursultonov-ops <ozodbeknursultonov@gmail.com>
2026-09-24 10:36:46 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8752164498:AAEoXKMvehRIbnC5yVgSw4N64E2Vc-4sL_w")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "8841439105"))
-DATABASE_URL = os.getenv("DATABASE_URL", "")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://baza_45sz_user:tpBvOCtwdb9YKOokJrwNfcEsN5DzbN8i@dpg-daqbcrk9v7es73cih7mg-a.oregon-postgres.render.com/baza_45sz")
